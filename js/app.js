be6549eb5663e76c1a6ccce18036d9063b07b482
tonypeanut <tonypeanut93@gmail.com>
2024-02-21 16:06:34 -0700

    Se adapta para sync await

--- js/app.js
+++ js/app.js
@@ -45,18 +45,29 @@ function mostrarAlerta(mensaje){
     }
 }
 
-function buscarImagenes(){
+async function buscarImagenes(){
     const termino = document.querySelector('#termino').value;
 
     const key= '42479425-85ecf530a7ebd05393e518d8e';
     const url = `https://pixabay.com/api/?key=${key}&q=${termino}&per_page=${registrosPorPagina}&page=${paginaActual}`
 
+    /*
     fetch(url)
         .then(respuesta => respuesta.json())
         .then(resultado => {
             totalPaginas = calcularPaginas(resultado.totalHits);
             mostrarImagenes(resultado.hits)
         })
+    */
+
+    try {
+        const respuesta = await fetch(url);
+        const resultado = await respuesta.json();
+        totalPaginas = calcularPaginas(resultado.totalHits);
+        mostrarImagenes(resultado.hits)
+    } catch (error) {
+        console.log(error);
+    }
 }
 
 // Generador que va registrar la cantidad de elementos de acuerdo a las paginas
